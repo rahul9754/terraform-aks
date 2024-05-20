8df76a270c5bbb93447aab93a9b25a91845c4921
rahulgupta9270 <rahulgupta12786@gmail.com>
2024-05-20 13:58:32 +0530

    removing comments from providers.tf

--- providers.tf
+++ providers.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "st-backend"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "sttfstate528745"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "st-backend"  
+    storage_account_name = "sttfstate528745"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
 
